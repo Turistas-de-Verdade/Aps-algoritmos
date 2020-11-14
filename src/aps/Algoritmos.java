ec9527681f686b0fda0c0393e749420cb235f1ae
Felipe-Ferreira-Dev <felipe.ferreira.sco@gmail.com>
2020-11-13 23:33:02 -0300

    quicksort

--- src/aps/Algoritmos.java
+++ src/aps/Algoritmos.java
@@ -158,4 +158,56 @@ public class Algoritmos {
 
 	/******************************************************************/
 
+	/** QUICK SORT */
+	public static void quickSort(int array[], int esq, int dir) {
+		if (esq < dir) {
+			int j = separar(array, esq, dir);
+			quickSort(array, esq, j - 1);
+			quickSort(array, j + 1, dir);
+		}
+
+	}
+
+	private static int separar(int array[], int esq, int dir) {
+
+		int i = esq + 1;
+		int j = dir;
+		int pivo = array[esq];
+		while (i <= j) {
+			if (array[i] <= pivo) {
+				i++;
+			} else if (array[j] > pivo) {
+				j--;
+			} else if (i <= j) {
+				trocar(array, i, j);
+				i++;
+				j--;
+			}
+		}
+		trocar(array, esq, j);
+		return j;
+	}
+
+	private static void trocar(int[] array, int i, int j) {
+		int aux = array[i];
+		array[i] = array[j];
+		array[j] = aux;
+	}
+
+	public static void chamaQuickSort(int tamArray) {
+		int array[] = new int[tamArray];
+
+		populaArray(array);
+
+		System.out.println("Array antes do QuickSort:");
+		System.out.println(Arrays.toString(array));
+
+		quickSort(array, 0, (array.length - 1));
+
+		System.out.println("Array depois do QuickSort:");
+		System.out.println(Arrays.toString(array));
+
+	}
+	/***********************************************************************/
+
 }
--- src/aps/ApsApplication.java
+++ src/aps/ApsApplication.java
@@ -12,56 +12,58 @@ public class ApsApplication {
 		}
 	}
 
-	public static void mergeSort(int array[], int arrayAux[], int ini, int fim) {
-		if (ini < fim) {
-			int meio = (ini + fim) / 2;
-			mergeSort(array, arrayAux, ini, meio);
-			mergeSort(array, arrayAux, meio + 1, fim);
-			intercalar(array, arrayAux, ini, meio, fim);
+	public static void quickSort(int array[], int esq, int dir) {
+		if (esq < dir) {
+			int j = separar(array, esq, dir);
+			quickSort(array, esq, j - 1);
+			quickSort(array, j + 1, dir);
 		}
 
 	}
 
-	private static void intercalar(int[] array, int[] arrayAux, int ini, int meio, int fim) {
-		for (int k = ini; k <= fim; k++) {
-			arrayAux[k] = array[k];
-		}
+	private static int separar(int array[], int esq, int dir) {
 
-		int i = ini;
-		int j = meio + 1;
-
-		for (int k = ini; k <= fim; k++) {
-			if (i > meio) {
-				array[k] = arrayAux[j++];
-			} else if (j > fim) {
-				array[k] = arrayAux[i++];
-			} else if (arrayAux[i] < arrayAux[j]) {
-				array[k] = arrayAux[i++];
-			} else {
-				array[k] = arrayAux[j++];
+		int i = esq + 1;
+		int j = dir;
+		int pivo = array[esq];
+		while (i <= j) {
+			if (array[i] <= pivo) {
+				i++;
+			} else if (array[j] > pivo) {
+				j--;
+			} else if (i <= j) {
+				trocar(array, i, j);
+				i++;
+				j--;
 			}
 		}
+		trocar(array, esq, j);
+		return j;
 	}
 
-	public static void chamaMergeSort(int tamArray) {
+	private static void trocar(int[] array, int i, int j) {
+		int aux = array[i];
+		array[i] = array[j];
+		array[j] = aux;
+	}
+
+	public static void chamaQuickSort(int tamArray) {
 		int array[] = new int[tamArray];
-		int arrayAux[] = new int[array.length];
 
 		populaArray(array);
 
-		System.out.println("Array antes do MergeSort:");
+		System.out.println("Array antes do QuickSort:");
 		System.out.println(Arrays.toString(array));
 
-		mergeSort(array, arrayAux, 0, (array.length - 1));
+		quickSort(array, 0, (array.length - 1));
 
-		System.out.println("Array depois do MergeSort:");
+		System.out.println("Array depois do QuickSort:");
 		System.out.println(Arrays.toString(array));
 
 	}
 
 	public static void main(String[] args) {
-		
-
+		chamaQuickSort(10);
 	}
 
 }
