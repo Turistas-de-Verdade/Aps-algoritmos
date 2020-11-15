14e0720d3322658145b8570ac42501b86d7f19d7
Felipe-Ferreira-Dev <felipe.ferreira.sco@gmail.com>
2020-11-15 17:12:49 -0300

    bucket sort

--- src/aps/Algoritmos.java
+++ src/aps/Algoritmos.java
@@ -2,15 +2,17 @@ package aps;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class Algoritmos {
-	private static final int VALOR_MAX = 100;
+	private static final int VALOR_MAX = 250;
+	private static final int TAM_BALDE = 15;
 
 	public static void populaArray(int array[]) {
 		Random r = new Random();
 		for (int i = 0; i < array.length; i++) {
-			array[i] = (r.nextInt() * 100000);
+			array[i] = (r.nextInt() * VALOR_MAX);
 		}
 	}
 
@@ -308,7 +310,7 @@ public class Algoritmos {
 
 	/** RADIX SORT */
 
-	public static void radisSort(int array[]) {
+	public static void radixSort(int array[]) {
 		Integer maior = encontraMaior(array);
 
 		int casasMaior = maior.toString().length();
@@ -361,7 +363,78 @@ public class Algoritmos {
 		System.out.println("Array antes do Radix Sort:");
 		System.out.println(Arrays.toString(array));
 
-		radisSort(array);
+		radixSort(array);
+
+		System.out.println("Array depois do Radix Sort:");
+		System.out.println(Arrays.toString(array));
+	}
+
+	/***********************************************/
+
+	/** BUCKET SORT */
+
+	public static void bucketSort(int array[], int tBalde) {
+
+		int numBaldes = (int) tBalde / 5;
+
+		LinkedList[] B = new LinkedList[numBaldes];
+
+		for (int i = 0; i < numBaldes; i++) {
+			B[i] = new LinkedList<Integer>();
+		}
+
+		for (int i = 0; i < array.length; i++) {
+			int j = numBaldes - 1;
+			while (true) {
+				if (j < 0) {
+					break;
+				}
+				if (array[i] >= (j * 5)) {
+					B[j].add(array[i]);
+					break;
+				}
+				j--;
+			}
+		}
+
+		int indice = 0;
+		for (int i = 0; i < numBaldes; i++) {
+
+			int[] arrayAux = new int[B[i].size()];
+
+			for (int j = 0; j < arrayAux.length; j++) {
+				arrayAux[j] = (Integer) B[i].get(j);
+			}
+
+			ordenaBalde(arrayAux);
+			for (int j = 0; j < arrayAux.length; j++, indice++) {
+				array[indice] = arrayAux[j];
+			}
+		}
+	}
+
+	public static void ordenaBalde(int array[]) {
+		int n = array.length;
+		for (int j = 1; j < n; j++) {
+			int key = array[j];
+			int k = j - 1;
+			while ((k > -1) && (array[k] > key)) {
+				array[k + 1] = array[k];
+				k--;
+			}
+			array[k + 1] = key;
+		}
+	}
+
+	public static void chamaBucket(int tamArray) {
+		int array[] = new int[tamArray];
+
+		populaArray(array);
+
+		System.out.println("Array antes do Radix Sort:");
+		System.out.println(Arrays.toString(array));
+
+		bucketSort(array, TAM_BALDE);
 
 		System.out.println("Array depois do Radix Sort:");
 		System.out.println(Arrays.toString(array));
--- src/aps/ApsApplication.java
+++ src/aps/ApsApplication.java
@@ -2,16 +2,18 @@ package aps;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class ApsApplication {
 
 	/**
-	 * to do bucketsort radixsort
+	 * to do bucketsort
 	 * 
 	 */
 
 	private static final int VALOR_MAX = 10000;
+	private static final int TAM_BALDE = 15;
 
 	public static void populaArray(int array[]) {
 		Random r = new Random();
@@ -20,52 +22,60 @@ public class ApsApplication {
 		}
 	}
 
-	public static void radisSort(int array[]) {
-		Integer maior = encontraMaior(array);
+	public static void bucketSort(int array[], int tBalde) {
 
-		int casasMaior = maior.toString().length();
+		int numBaldes = (int) tBalde / 5;
 
-		for (int casa = 1; casa <= casasMaior; casa++) {
-			ArrayList<Integer> arrayList[] = new ArrayList[10];
+		LinkedList[] B = new LinkedList[numBaldes];
 
-			for (int i = 0; i < arrayList.length; i++) {
-				arrayList[i] = new ArrayList<Integer>();
-			}
-
-			for (int num : array) {
-				int mod = (int) Math.pow(10, casa);
-				int div = (int) mod / 10;
-				int lsd = (num % mod) / div;
-
-				arrayList[lsd].add(num);
-
-			}
-
-			ArrayList<Integer> arrayListAux = new ArrayList<Integer>();
-			for (ArrayList<Integer> ls : arrayList) {
-				for (Integer x : ls) {
-					arrayListAux.add(x);
-				}
-			}
-			for (int i = 0; i < arrayListAux.size(); i++) {
-				array[i] = arrayListAux.get(i);
-			}
-
+		for (int i = 0; i < numBaldes; i++) {
+			B[i] = new LinkedList<Integer>();
 		}
-
-	}
-
-	public static Integer encontraMaior(int array[]) {
-		Integer maior = 0;
 
 		for (int i = 0; i < array.length; i++) {
-			maior = (array[i] > maior) ? array[i] : maior;
+			int j = numBaldes - 1;
+			while (true) {
+				if (j < 0) {
+					break;
+				}
+				if (array[i] >= (j * 5)) {
+					B[j].add(array[i]);
+					break;
+				}
+				j--;
+			}
 		}
 
-		return maior;
+		int indice = 0;
+		for (int i = 0; i < numBaldes; i++) {
+
+			int[] arrayAux = new int[B[i].size()];
+
+			for (int j = 0; j < arrayAux.length; j++) {
+				arrayAux[j] = (Integer) B[i].get(j);
+			}
+
+			ordenaBalde(arrayAux);
+			for (int j = 0; j < arrayAux.length; j++, indice++) {
+				array[indice] = arrayAux[j];
+			}
+		}
 	}
 
-	public static void chamaRadix(int tamArray) {
+	public static void ordenaBalde(int array[]) {
+		int n = array.length;
+		for (int j = 1; j < n; j++) {
+			int key = array[j];
+			int k = j - 1;
+			while ((k > -1) && (array[k] > key)) {
+				array[k + 1] = array[k];
+				k--;
+			}
+			array[k + 1] = key;
+		}
+	}
+
+	public static void chamaBucket(int tamArray) {
 		int array[] = new int[tamArray];
 
 		populaArray(array);
@@ -73,13 +83,15 @@ public class ApsApplication {
 		System.out.println("Array antes do Radix Sort:");
 		System.out.println(Arrays.toString(array));
 
-		radisSort(array);
+		bucketSort(array, TAM_BALDE);
 
 		System.out.println("Array depois do Radix Sort:");
 		System.out.println(Arrays.toString(array));
 	}
 
 	public static void main(String[] args) {
-		chamaRadix(15);
+
+		chamaBucket(50);
+
 	}
 }
