0bb8e1af8090b0188b6e622d5b24c83ad7396b5a
Felipe-Ferreira-Dev <felipe.ferreira.sco@gmail.com>
2020-11-15 19:04:10 -0300

    teste 2.3

--- src/aps/ApsApplication.java
+++ src/aps/ApsApplication.java
@@ -4,79 +4,80 @@ import java.util.ArrayList;
 
 public class ApsApplication {
 
-	ArrayList<Long> tempoDeExecArray = new ArrayList<Long>();
-	
+	static ArrayList<Long> tempoDeExecArray = new ArrayList<Long>();
+
 	public static void testaAlgoritmos() {
 		for (int i = 0; i < 50; i++) {
-		
-			Algoritmos.chamaInsertionSort(5);
-			Algoritmos.chamaInsertionSort(10);
-			Algoritmos.chamaInsertionSort(50);
-			Algoritmos.chamaInsertionSort(100);
-			Algoritmos.chamaInsertionSort(1000);
-			Algoritmos.chamaInsertionSort(10000);
 
-			Algoritmos.chamaSelectionSort(5);
-			Algoritmos.chamaSelectionSort(10);
-			Algoritmos.chamaSelectionSort(50);
-			Algoritmos.chamaSelectionSort(100);
-			Algoritmos.chamaSelectionSort(1000);
-			Algoritmos.chamaSelectionSort(10000);
+			tempoDeExecArray.add(i, Algoritmos.chamaInsertionSort(5));
+			tempoDeExecArray.add(i, Algoritmos.chamaInsertionSort(10));
+			tempoDeExecArray.add(i, Algoritmos.chamaInsertionSort(50));
+			tempoDeExecArray.add(i, Algoritmos.chamaInsertionSort(100));
+			tempoDeExecArray.add(i, Algoritmos.chamaInsertionSort(1000));
+			tempoDeExecArray.add(i, Algoritmos.chamaInsertionSort(10000));
 
-			Algoritmos.chamaBubbleSort(5);
-			Algoritmos.chamaBubbleSort(10);
-			Algoritmos.chamaBubbleSort(50);
-			Algoritmos.chamaBubbleSort(100);
-			Algoritmos.chamaBubbleSort(1000);
-			Algoritmos.chamaBubbleSort(10000);
+			tempoDeExecArray.add(i, Algoritmos.chamaSelectionSort(5));
+			tempoDeExecArray.add(i, Algoritmos.chamaSelectionSort(10));
+			tempoDeExecArray.add(i, Algoritmos.chamaSelectionSort(50));
+			tempoDeExecArray.add(i, Algoritmos.chamaSelectionSort(100));
+			tempoDeExecArray.add(i, Algoritmos.chamaSelectionSort(1000));
+			tempoDeExecArray.add(i, Algoritmos.chamaSelectionSort(10000));
 
-			Algoritmos.chamaMergeSort(5);
-			Algoritmos.chamaMergeSort(10);
-			Algoritmos.chamaMergeSort(50);
-			Algoritmos.chamaMergeSort(100);
-			Algoritmos.chamaMergeSort(1000);
-			Algoritmos.chamaMergeSort(10000);
+			tempoDeExecArray.add(i, Algoritmos.chamaBubbleSort(5));
+			tempoDeExecArray.add(i, Algoritmos.chamaBubbleSort(10));
+			tempoDeExecArray.add(i, Algoritmos.chamaBubbleSort(50));
+			tempoDeExecArray.add(i, Algoritmos.chamaBubbleSort(100));
+			tempoDeExecArray.add(i, Algoritmos.chamaBubbleSort(1000));
+			tempoDeExecArray.add(i, Algoritmos.chamaBubbleSort(10000));
 
-			Algoritmos.chamaQuickSort(5);
-			Algoritmos.chamaQuickSort(10);
-			Algoritmos.chamaQuickSort(50);
-			Algoritmos.chamaQuickSort(100);
-			Algoritmos.chamaQuickSort(1000);
-			Algoritmos.chamaQuickSort(10000);
+			tempoDeExecArray.add(i, Algoritmos.chamaMergeSort(5));
+			tempoDeExecArray.add(i, Algoritmos.chamaMergeSort(10));
+			tempoDeExecArray.add(i, Algoritmos.chamaMergeSort(50));
+			tempoDeExecArray.add(i, Algoritmos.chamaMergeSort(100));
+			tempoDeExecArray.add(i, Algoritmos.chamaMergeSort(1000));
+			tempoDeExecArray.add(i, Algoritmos.chamaMergeSort(10000));
 
-			Algoritmos.chamaHeapSort(5);
-			Algoritmos.chamaHeapSort(10);
-			Algoritmos.chamaHeapSort(50);
-			Algoritmos.chamaHeapSort(100);
-			Algoritmos.chamaHeapSort(1000);
-			Algoritmos.chamaHeapSort(10000);
+			tempoDeExecArray.add(i, Algoritmos.chamaQuickSort(5));
+			tempoDeExecArray.add(i, Algoritmos.chamaQuickSort(10));
+			tempoDeExecArray.add(i, Algoritmos.chamaQuickSort(50));
+			tempoDeExecArray.add(i, Algoritmos.chamaQuickSort(100));
+			tempoDeExecArray.add(i, Algoritmos.chamaQuickSort(1000));
+			tempoDeExecArray.add(i, Algoritmos.chamaQuickSort(10000));
 
-			Algoritmos.chamaCountingSort(5);
-			Algoritmos.chamaCountingSort(10);
-			Algoritmos.chamaCountingSort(50);
-			Algoritmos.chamaCountingSort(100);
-			Algoritmos.chamaCountingSort(1000);
-			Algoritmos.chamaCountingSort(10000);
+			tempoDeExecArray.add(i, Algoritmos.chamaHeapSort(5));
+			tempoDeExecArray.add(i, Algoritmos.chamaHeapSort(10));
+			tempoDeExecArray.add(i, Algoritmos.chamaHeapSort(50));
+			tempoDeExecArray.add(i, Algoritmos.chamaHeapSort(100));
+			tempoDeExecArray.add(i, Algoritmos.chamaHeapSort(1000));
+			tempoDeExecArray.add(i, Algoritmos.chamaHeapSort(10000));
 
-			Algoritmos.chamaRadixSort(5);
-			Algoritmos.chamaRadixSort(10);
-			Algoritmos.chamaRadixSort(50);
-			Algoritmos.chamaRadixSort(100);
-			Algoritmos.chamaRadixSort(1000);
-			Algoritmos.chamaRadixSort(10000);
+			tempoDeExecArray.add(i, Algoritmos.chamaCountingSort(5));
+			tempoDeExecArray.add(i, Algoritmos.chamaCountingSort(10));
+			tempoDeExecArray.add(i, Algoritmos.chamaCountingSort(50));
+			tempoDeExecArray.add(i, Algoritmos.chamaCountingSort(100));
+			tempoDeExecArray.add(i, Algoritmos.chamaCountingSort(1000));
+			tempoDeExecArray.add(i, Algoritmos.chamaCountingSort(10000));
 
-			Algoritmos.chamaBucketSort(5);
-			Algoritmos.chamaBucketSort(10);
-			Algoritmos.chamaBucketSort(50);
-			Algoritmos.chamaBucketSort(100);
-			Algoritmos.chamaBucketSort(1000);
-			Algoritmos.chamaBucketSort(10000);
+			tempoDeExecArray.add(i, Algoritmos.chamaRadixSort(5));
+			tempoDeExecArray.add(i, Algoritmos.chamaRadixSort(10));
+			tempoDeExecArray.add(i, Algoritmos.chamaRadixSort(50));
+			tempoDeExecArray.add(i, Algoritmos.chamaRadixSort(100));
+			tempoDeExecArray.add(i, Algoritmos.chamaRadixSort(1000));
+			tempoDeExecArray.add(i, Algoritmos.chamaRadixSort(10000));
+
+			tempoDeExecArray.add(i, Algoritmos.chamaBucketSort(5));
+			tempoDeExecArray.add(i, Algoritmos.chamaBucketSort(10));
+			tempoDeExecArray.add(i, Algoritmos.chamaBucketSort(50));
+			tempoDeExecArray.add(i, Algoritmos.chamaBucketSort(100));
+			tempoDeExecArray.add(i, Algoritmos.chamaBucketSort(1000));
+			tempoDeExecArray.add(i, Algoritmos.chamaBucketSort(10000));
 		}
-		
-		
+
 	}
 
 	public static void main(String[] args) {
 		testaAlgoritmos();
+
+		System.out.println(tempoDeExecArray);
 	}
 }
